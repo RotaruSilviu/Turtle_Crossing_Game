dd8ea796edd6f9fe56fff7cbba2d9aa0060304db
RotaruSilviu <159648340+RotaruSilviu@users.noreply.github.com>
2024-02-12 21:02:49 +0200

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -8,15 +8,11 @@ SPEED_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
-        ### Ascundem testoasa care se creaza in mijlocul ecranului.
         self.hideturtle()
         self.cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
 
-    # Functie ca sa cream masinilie.
     def create_car(self):
-        ### IN felul acesta am creat o sansa aleatorie, asa ca e o sansa 1/6 ca sa se faca o masina,
-        # astfel aparand mai rar pe ecran.
         random_chance = random.randint(1, 6)
         if random_chance == 1:
             new_car = Turtle(shape="square")
@@ -28,12 +24,9 @@ class CarManager(Turtle):
             new_car.goto(300, random.randint(-250, 250))
             self.cars.append(new_car)
 
-    ### Functie ca sa mutam masinile.
     def move_cars(self,):
         for car in self.cars:
             car.forward(self.car_speed)
-            ### Aici dispare masina odata ce ajunge la final si e scoasa din lista de masini.
-            # Deoarece masinile se regenereaza continuu , lista cu masini nu se termina niciodata.
             if car.xcor() < -300:
                 car.hideturtle()
                 self.cars.remove(car)
